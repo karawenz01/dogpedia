b8aa3ca60c43ce0ade679c83497d6e04577e47b9
KWenz <karawenz01@gmail.com>
2018-11-14 22:34:26 -0800

    breed recognization

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ from flask import Flask, jsonify, render_template, request, redirect
 from flask_pymongo import PyMongo
 import pymongo
 
+import keras
+from keras.preprocessing import image
+from keras import backend as K
+# from keras.genetic_utils import CustomObjectScope
+
+
 application = Flask(__name__)
 
 # 1. Database setup
@@ -160,6 +166,68 @@ def findBreed():
     }
 
     return jsonify(value)
+
+
+# application.config['UPLOAD_FOLDER'] = 'Uploads'
+
+# model = None
+# graph = None
+
+
+# Loading a keras model with flask
+# https://blog.keras.io/building-a-simple-keras-deep-learning-rest-api.html
+# def load_model():
+#     global model
+#     global graph
+   
+#     model = keras.models.load_model("breed_recognition.h5")
+#     graph = K.get_session().graph
+
+
+# load_model()
+
+# @app.route('/', methods=['GET', 'POST'])
+# def upload_file():
+#     data = {"success": False}
+#     if request.method == 'POST':
+#         print(request)
+
+#         if request.files.get('file'):
+#             # read the file
+#             file = request.files['file']
+
+#             # read the filename
+#             filename = file.filename
+
+#             # create a path to the uploads folder
+#             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+#             # Save the file to the uploads folder
+#             file.save(filepath)
+
+#             # Load the saved image using Keras and resize it to the mnist
+#             # format of 28x28 pixels
+#             # image_size = (28, 28)
+#             # im = image.load_img(filepath, target_size=image_size,
+#             #                     grayscale=True)
+
+#             # Convert the 2D image to an array of pixel values
+#             # image_array = prepare_image(im)
+#             # print(image_array)
+
+#             # Get the tensorflow default graph and use it to make predictions
+#             global graph
+#             with graph.as_default():
+
+#                 # Use the model to make a prediction
+#                 predicted_digit = model.predict_classes(image_array)[0]
+#                 data["prediction"] = str(predicted_digit)
+
+#                 # indicate that the request was a success
+#                 data["success"] = True
+
+#             return jsonify(data)
+
 
 
 if __name__ == "__main__":
--- static/js/find_logic.js
+++ static/js/find_logic.js
@@ -1,59 +1,5 @@
-// Creating map object
-var myMap = L.map('map').setView([37.8, -96], 4);
-
-// adding tile layer
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + API_KEY, {
-    id: 'mapbox.streets',
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoon: 18
-}).addTo(myMap);
-
 console.log("I am working!")
 
-// Link to GeoJSON
-var StatesData
-
-d3.json("/states").then(data => {
-    StatesData = L.geoJson(data).addTo(myMap);
-    StatesData.addData(geojsonFeature);  
-    console.log(data)  
-}) 
-
-
-var geojson;
-
-// Use the pet_stores geojson data
-d3.json('/states', function(data) {
-    console.log(data);
-
-    // Create a new choropleth layer
-    geojson = L.choropleth(data, {
-
-        // Define what property in the features to use
-        valueProperty: "BUSINESS",
-
-        // Set color scale
-        scale: ["#ffffb2", "#b10026"],
-
-        // Number of breaks in step range
-        steps: 100,
-
-        // q for quartile, e for equidistant, k for k-means
-        // mode: "q",
-        style: {
-            // Border color
-            color: "#fff",
-            weight: 1,
-            fillOpacity: 0.8
-        },
-    
-        // Binding a pop-up to each layer
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.NAME + ": " + feature.properties.BUSINESS);
-        }
-    }).addTo(myMap);
-
-});
 
 /* Kara and McKenzi Worked
 // Survey variable
